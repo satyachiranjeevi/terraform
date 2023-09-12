feff88a3cf2b3576fc5f226b2ddfe82e87c916d0
satyachiranjeevi <duvvu.chiranjeevi@gmail.com>
2023-09-12 21:42:19 +0100

    variables

--- session01/ec2.tf
+++ session01/ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "demo-terraform" {
-    ami = variable.ami_id #devops practice in us-east-1
-    instance_type = variable.instance_type
+    ami = var.ami_id
+    instance_type = var.instance_type
 }
